78ce1d10f799e93828ab213ea51e1031d982cb51
Mr_dreamer <alexndreamer@gmail.com>
2020-01-03 18:13:59 +0800

    增加用户名验证

--- app/controller/SignUp.php
+++ app/controller/SignUp.php
@@ -17,12 +17,30 @@ use app\model\User,
 
 class SignUp extends BaseController
 {
+    //判断用户名是否存在
+    public function isUserExist()
+    {
+        $User = new User();
+        $data = Request::only(['username']);
+        if ($User->isUserExist($data['username'])) {
+            $this->response([], 555, '用户名已被占用');
+        }else{
+            $this->response();
+        }
+    }
+
     //注册
     public function signUp()
     {
         $data = Request::only(['username', 'password', 'nickname']);
         $User = new User();
 
+        if ($User->isUserExist($data['username'])) {
+            $this->response([], 555, '用户名已被占用');
+
+        }
+
+        $data['password'] = password_hash($data['password'],1);
         $data['create_time'] = date('Y-m-d H:i:s');
 
         if ($User->save($data)) {
